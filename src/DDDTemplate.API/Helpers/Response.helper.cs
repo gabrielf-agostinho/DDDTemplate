40cabe15f77b909a10b55fc45b2c991c306f6844
Gabriel Agostinho <g.felipeagostinho@gmail.com>
2026-03-23 16:06:19 -0300

    refactor: New Response pattern

--- src/DDDTemplate.API/Helpers/Response.helper.cs
+++ src/DDDTemplate.API/Helpers/Response.helper.cs
@@ -3,34 +3,30 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DDDTemplate.API.Helpers;
 
-public class Response(EResponseCodes code, object? data = null, string? errorMessage = null) : IActionResult
+public class Response(EResponseCodes code, object? data = null, string? error = null) : IActionResult
 {
-  public EResponseCodes Code { get; } = code;
+  public bool Success { get; } = (int)code >= 200 && (int)code <= 299;
   public object? Data { get; } = data;
-  public string? ErrorMessage { get; } = errorMessage;
-
-  public bool Success => (int)Code >= (int)EResponseCodes.OK && (int)Code <= (int)EResponseCodes.MULTI_STATUS;
+  public string? Error { get; } = error;
+  public EResponseCodes Code { get; } = code;
 
   public Response(EResponseCodes code, Exception exception) : this(code, null, exception.Message) { }
 
   public async Task ExecuteResultAsync(ActionContext context)
   {
-    var responseContent = Success ? Data : new { Error = ErrorMessage };
-
-    if (responseContent is null)
+    var result = new
     {
-      var statusResult = new StatusCodeResult((int)Code);
+      success = Success,
+      data = Data,
+      error = Error,
+      code = (int)Code
+    };
 
-      await statusResult.ExecuteResultAsync(context);
-    }
-    else
+    var objectResult = new ObjectResult(result)
     {
-      var objectResult = new ObjectResult(responseContent)
-      {
-        StatusCode = (int)Code
-      };
+      StatusCode = (int)Code
+    };
 
-      await objectResult.ExecuteResultAsync(context);
-    }
+    await objectResult.ExecuteResultAsync(context);
   }
 }
